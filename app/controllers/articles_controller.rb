4f6f73d6acd54e31b015e05d46319cb10c75ef6b
Rails Udemy Course <railsudemycourse@gmail.com>
2019-02-12 14:22:06 +0100

    Add partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,15 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
@@ -12,12 +21,28 @@ class ArticlesController < ApplicationController
         else
             render "new"
         end
-        #@article.save
-        #redirect_to article_path(@article)
+
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was sucessfully updated"
+            redirect_to article_path(@article)
+        else
+            render "edit"
+        end
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was sucessfully deleted"
+        redirect_to articles_path
     end
 
     private
