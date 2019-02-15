6279f4440fe69feac09a3b2d7ad966c7bcbb722e
Rails Udemy Course <railsudemycourse@gmail.com>
2019-02-15 11:25:46 +0100

    changed articles controller removed debugger line

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
         flash[:success] = "article was sucessfuly created"
             redirect_to article_path(@article)
