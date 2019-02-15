40b13728436eeb28a79ebc1a6ae98319e7c6e00c
Rails Udemy Course <railsudemycourse@gmail.com>
2019-02-15 11:20:52 +0100

    add associaation betw user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,9 @@ class ArticlesController < ApplicationController
 
 
     def create
+        #debugger
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
         flash[:success] = "article was sucessfuly created"
             redirect_to article_path(@article)
