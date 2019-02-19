75cfea1704f2b92ba78decd74b7ba6674249e47a
Rails Udemy Course <railsudemycourse@gmail.com>
2019-02-19 09:32:53 +0100

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show] # %i(index show)
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @article = Article.new
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = User.last
         if @article.save
         flash[:success] = "article was sucessfuly created"
             redirect_to article_path(@article)
@@ -54,5 +56,11 @@ class ArticlesController < ApplicationController
         end
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own article"
+                redirect_to root_path
+            end
         end
 end
